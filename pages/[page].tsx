84f73420af1c5210cf8994b0b93bb17c59409089
Mauricio Robayo <mauricio.robayo@kinandcarta.com>
2023-04-17 15:26:12 -0500

    update config

--- pages/[page].tsx
+++ pages/[page].tsx
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { onEntryChange } from '../contentstack-sdk';
-import RenderComponents from '../components/render-components';
-import { getPageRes } from '../helper';
-import Skeleton from 'react-loading-skeleton';
+import React, { useState, useEffect } from "react";
+import { onEntryChange } from "../contentstack-sdk";
+import RenderComponents from "../components/render-components";
+import { getPageRes } from "../helper";
+import Skeleton from "react-loading-skeleton";
 import { Props } from "../typescript/pages";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-export default function Page(props: Props) {
+export default function Page(props: Props & { time: number }) {
   const { page, entryUrl } = props;
   const [getEntry, setEntry] = useState(page);
-
+  console.log("time from server", props.time);
   async function fetchData() {
     try {
       const entryRes = await getPageRes(entryUrl);
-      if (!entryRes) throw new Error('Status code 404');
+      if (!entryRes) throw new Error("Status code 404");
       setEntry(entryRes);
     } catch (error) {
       console.error(error);
@@ -24,30 +25,59 @@ export default function Page(props: Props) {
   }, [page]);
 
   return getEntry.page_components ? (
-    <RenderComponents
-      pageComponents={getEntry.page_components}
-      contentTypeUid='page'
-      entryUid={getEntry.uid}
-      locale={getEntry.locale}
-    />
+    <>
+      <RenderComponents
+        pageComponents={getEntry.page_components}
+        contentTypeUid="page"
+        entryUid={getEntry.uid}
+        locale={getEntry.locale}
+      />
+    </>
   ) : (
     <Skeleton count={3} height={300} />
   );
 }
 
-export async function getServerSideProps({params}: any) {
-  try {
-      const entryUrl = params.page.includes('/') ? params.page:`/${params.page}`
-      const entryRes = await getPageRes(entryUrl);
-      if (!entryRes) throw new Error('404');
-      return {
-        props: {
-          entryUrl: entryUrl,
-          page: entryRes,
-        },
-      };
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
 
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
+  try {
+    const entryUrl = params.page.includes("/")
+      ? params.page
+      : `/${params.page}`;
+    const entryRes = await getPageRes(entryUrl);
+    if (!entryRes) throw new Error("404");
+    return {
+      props: {
+        entryUrl: entryUrl,
+        page: entryRes,
+        time: Date.now(),
+      },
+      revalidate: 1,
+    };
   } catch (error) {
     return { notFound: true };
   }
-}
+};
+
+// export async function getServerSideProps({params}: any) {
+//   try {
+//       const entryUrl = params.page.includes('/') ? params.page:`/${params.page}`
+//       const entryRes = await getPageRes(entryUrl);
+//       if (!entryRes) throw new Error('404');
+//       return {
+//         props: {
+//           entryUrl: entryUrl,
+//           page: entryRes,
+//         },
+//       };
+
+//   } catch (error) {
+//     return { notFound: true };
+//   }
+// }
